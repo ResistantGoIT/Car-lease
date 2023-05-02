126b66cc4ff1eb2ab965c61961d3ddd5012425ea
Vitaliy Protasevych <vitaliy.protasevych@gmail.com>
2023-05-02 16:50:27 +0300

    adaptive
    some adaptive 1th page

--- js/main.js
+++ js/main.js
@@ -23,6 +23,14 @@ function open(evt) {
 }
 
 
+const menuBtn = document.querySelector('.menu__btn');
+const menu = document.querySelector('.menu__list');
+
+menuBtn.addEventListener('click', ()=> {
+  menu.classList.toggle('menu__list--actve')
+})
+
+
 const swiper = new Swiper(".swiper", {
    pagination: {
      el: ".swiper-pagination",
